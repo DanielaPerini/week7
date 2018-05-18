ab4e23d7ec874ee15fc1c82d528408db6e253280
DLPerini <daniela.perini123@gmail.com>
2018-05-17 19:35:14 -0700

    multiple charts

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ console.log("let's build some charts!")
       google.charts.load('current', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
+      google.charts.setOnLoadCallback(drawChart1);      
+      google.charts.setOnLoadCallback(drawChart2);
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
-      function drawChart() {
+      function drawChart1() {
 
         // Create the data table.
         var data = new google.visualization.DataTable();
@@ -24,9 +25,38 @@ console.log("let's build some charts!")
         // Set chart options
         var options = {'title':'What kinds of pizza I ate last night',
                        'width':400,
-                       'height':300};
+                       'height':300,
+                       'legend':'left',
+                       'is3D':true
+                      };
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+        chart.draw(data, options);
+      }
+
+      function drawChart2() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 2],
+          ['Onions', 4],
+          ['Olives', 1],
+          ['Corn', 1]
+        ]);
+
+        // Set chart options
+        var options = {'title':'What kinds of pizza I ate last night',
+                       'width':400,
+                       'height':300,
+                       'legend':'left',
+                       'is3D':true
+                      };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
         chart.draw(data, options);
       }
